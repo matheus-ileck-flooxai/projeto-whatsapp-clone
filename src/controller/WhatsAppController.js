89922100349a428fd207f8e346aa59355402caee
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-03-26 12:10:21 -0300

    feat: add event to insert emojis into message

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -314,6 +314,100 @@ class WhatsAppController{
 
         })
 
+
+        this.el.inputText.on('keypress', e=>{
+
+
+            if(e.key === 'Enter' && !e.ctrlKey){
+
+                e.preventDefault();
+
+                this.el.btnSend.click();
+
+            }
+
+        });
+
+        this.el.inputText.on('keyup', e=>{
+
+            if(this.el.inputText.innerHTML.length && this.el.inputText.innerHTML !== '<br>'){
+
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+            }
+            else{
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+
+
+            }
+
+        });
+
+        this.el.btnSend.on('click', e=>{
+
+            console.log(this.el.inputText.innerHTML);
+            
+
+        });
+
+        this.el.btnEmojis.on('click', e=>{
+
+            this.el.panelEmojis.toggleClass('open');
+
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji=>{
+
+
+            emoji.on('click',e=>{
+
+                console.log(emoji.dataset.unicode);
+                let img = this.el.imgEmojiDefault.cloneNode()
+
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+                
+                emoji.classList.forEach(name=>{
+
+                    img.classList.add(name);
+
+                });
+
+                let cursor = window.getSelection();
+
+                if(!cursor.focusNode || !cursor.focusNode.id == 'input-Text'){
+
+                    this.el.inputText.focus();
+
+                    cursor = window.getSelection();
+
+                }
+
+                let range = document.createRange();
+
+                range = cursor.getRangeAt(0);
+                range.deleteContents();
+
+
+                let frag = document.createDocumentFragment();
+
+                frag.appendChild(img);
+
+                range.insertNode(frag);
+
+                range.setStartAfter(img);
+
+                this.el.inputText.dispatchEvent(new Event('keyup'));
+
+            })
+
+        });
+
+
     }
 
 
