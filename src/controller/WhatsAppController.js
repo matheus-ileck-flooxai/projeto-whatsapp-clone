ca07cd9f10cef3b00d45e1a17c2da9bfe52a4734
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-03-26 10:21:02 -0300

    feat: add formData to get form values

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -87,6 +87,24 @@ class WhatsAppController{
             return this.classList.contains(name);
         }
 
+        HTMLFormElement.prototype.getForm = function(){
+
+            return new FormData(this);
+        }
+        
+        HTMLFormElement.prototype.toJSON = function(){
+
+            let json = {};
+
+            this.getForm().forEach((value, key)=>{
+
+                json[key] = value;
+
+
+            });
+            return json;
+        }
+
     }
     initEvents(){
 
@@ -129,8 +147,41 @@ class WhatsAppController{
 
             this.el.panelAddContact.removeClass('open');
 
-        })
+        });
 
+        this.el.photoContainerEditProfile.on('click', e=>{
+
+            this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e=>{
+
+
+            if(e.key === 'Enter'){
+
+                e.preventDefault()
+
+                this.btnSavePanelEditProfile.click();
+
+            }
+
+        });
+        this.el.btnSavePanelEditProfile.on('click', e=>{
+
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e=>{
+
+
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact);
+            
+        });
     }
 
     closeAllLeftPanel(){
