964e43ec1fd7446d2f40a12359d15a621aea6dd1
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-03-26 11:08:38 -0300

    feat: add click events to attached menu items

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -207,21 +207,101 @@ class WhatsAppController{
 
         this.el.btnAttachPhoto.on('click', e=>{
 
+            this.el.inputPhoto.click();
+
+        });
+        this.el.inputPhoto.on('change', e=>{
+
             
-            console.log('Photo')
-        })
+            console.log(this.el.inputPhoto.files);
+
+            [this.el.inputPhoto.files].forEach(file=>{
+
+                console.log(file);
+
+            });
+        });
+        
+        
         this.el.btnAttachCamera.on('click', e=>{
 
-            console.log('Camera')
-        })
+            this.closeAllMainPanel();
+
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+
+                'height':'calc(100% - 2px)'
+
+            });
+
+
+        });
+
+        this.el.btnClosePanelCamera.on('click', e=>{
+
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnTakePicture.on('click', e=>{
+
+            console.log('Abriu');
+
+        });
         this.el.btnAttachDocument.on('click', e=>{
 
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+
+                'height':'calc(100% - 2px)'
+
+            });
             console.log('Document')
+
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click',e=>{
+
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+
+            
+        });
+
+        this.el.btnSendDocument.on('click',e=>{
+
+
+            console.log('teste');
+
         })
+
+
         this.el.btnAttachContact.on('click', e=>{
 
-            console.log('Contact')
+            this.el.modalContacts.show();
+
+        });
+
+        this.el.btnCloseModalContacts.on('click',e=>{
+
+
+            this.el.modalContacts.hide();
+
+
         })
+    }
+
+    closeAllMainPanel(){
+
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+
+
+
+
     }
 
     closeMenuAttach(e){
